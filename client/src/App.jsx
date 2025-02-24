b0b47e3b90cca532a5a5f3dc4125cdd72ae8de57
techie-neha <neha.kanki@rapidops.co>
2025-02-25 00:23:51 +0530

    few changes in client

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,19 @@
-import { useState } from 'react'
+import { useState,useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import axios from "axios"
 
 function App() {
   const [count, setCount] = useState(0)
+const fetchAPI =async()=>{
+  const res = await axios.get("http://localhost:3000/api");
+  console.log(res.data)
+}
 
+useEffect(()=>{
+  fetchAPI()
+},[])
   return (
     <>
       <div>
