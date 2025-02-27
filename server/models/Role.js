587ac9e7edd1c561ecef07f18ec6183ef71382fa
techie-neha <neha.kanki@rapidops.co>
2025-02-27 18:48:29 +0530

    server role delete model

--- server/models/Role.js
+++
@@ -1,8 +0,0 @@
-// const mongoose = require("mongoose");
-
-// const roleSchema = new mongoose.Schema({
-//     name: { type: String, required: true },
-//     // permissions: [String], // e.g., 'create:contact', 'manage:users'
-// }, { timestamps: true });
-
-// module.exports = mongoose.model("Role", roleSchema);
